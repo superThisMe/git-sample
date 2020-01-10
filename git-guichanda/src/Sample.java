8412d553fdac40edf540cefb9b9f09692f56ab59
superthisme <apprentice91@gmail.com>
2020-01-10 12:32:38 +0900

    졸려...

---
+++ git-guichanda/src/Sample.java
@@ -0,0 +1,11 @@
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("아무것도 하기 싫다.");
+		System.out.println("이미 아무것도 안 하고 있지만,");
+		System.out.println("더 격렬하게 아무것도 안 하고 싶다.");
+
+	}
+
+}
