5ebe948b21aac28fd906340b25f0f7bda8fbd189
superthisme <apprentice91@gmail.com>
2020-01-10 12:51:41 +0900

    New text "뷁!" inserted

--- git-guichanda/src/Sample.java
+++ git-guichanda/src/Sample.java
@@ -5,6 +5,9 @@ public class Sample {
 		System.out.println("아무것도 하기 싫다.");
 		System.out.println("이미 아무것도 안 하고 있지만,");
 		System.out.println("더 격렬하게 아무것도 안 하고 싶다.");
+		
+		System.out.println("----------------------");
+		System.out.println("뷁!");
 
 	}
 
